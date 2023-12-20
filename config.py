a11875f6a5c96831fcea6435cede599e20f730da
TARSHANBATMAN <103805948+TARSHANBATMAN@users.noreply.github.com>
2023-12-20 08:38:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ckumsfjls2cy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tarshan-employee"
+customregion = "us-east-1"
+
